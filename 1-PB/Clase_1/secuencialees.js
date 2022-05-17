682569e34ff5ab8be87ec42e11a13e9f4ec3b235
AnchorBase <https://github.com/francoRamirez453/AnchorBase.git>
2022-05-17 11:14:35 -0300

    Ejercicio_clase_1

---
+++ 1-PB/Clase_1/secuencialees.js
@@ -0,0 +1,3 @@
+var secuencia1;
+secuencia1 = "Comprar los productos.\n Romper 6  huevos en un boul y mesclarlo con una taza de azucar.\n Echarle a la mezcla de la azucar y los huevos,dos tazas de harina leudante.\n batir todo hasta que se una toda la mezcla.\n Poner la mezcla en un molde de bizcochuelo.\n Prender el horno y poner el bizcochuelo hasta que se cocine.\n Una vez cocinado dejar enfriar\n,rellenar y decorar el bizcochuelo. ";
+console.log(secuencia1);
