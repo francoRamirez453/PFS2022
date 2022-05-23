39c12450ae6c38bb6d78e6bda6186482cbf3eae2
AnchorBase <https://github.com/francoRamirez453/AnchorBase.git>
2022-05-23 09:46:40 -0300

    multiplos

---
+++ 1-PB/Clase_3/multiplo.ts
@@ -0,0 +1,16 @@
+let i: number;
+let j:number = 100;
+
+for(i = 1 ; i<= j; i++){
+    console.log(i)
+    if(i % 2 == 0){
+        console.log("Es multiplo de 2",i)
+
+    }
+    if(i % 3 == 0){
+        console.log("Es multiplo de 3", i)
+
+    }
+        
+    
+}
